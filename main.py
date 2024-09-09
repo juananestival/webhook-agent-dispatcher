c2cfc09db1ec8119909fcd3ab927a3d784ebe7fb
juananestival <juananestival@gmail.com>
2024-09-09 07:51:11 +0000

    no match

--- main.py
+++ main.py
@@ -51,6 +51,46 @@ def sample_wh():
   response_json = json.dumps(output)
   return response_json
   # return flask.jsonify({"results": "output"})
+@app.post("/nomatch")
+def nomatch():
+  data = request.get_json()
+  print(f"data {data}")
+  output = {
+    'sessionInfo': {
+      'parameters': {
+        'userAuthenticated': 'y',
+        }
+      },
+    "fulfillment_response": {
+      "messages": [
+        {
+          "text": {
+            "text": ["I understand that you  want to compare accounts. Click on the button if that is the case or type a new search."]
+            }
+        },
+        {
+          "payload": {
+            "richContent": [
+                        [
+                            {
+                                "type": "chips",
+                                "options": [
+                                    { "text": "I want to compare accounts" }
+
+                                ]
+                            }
+                        ]
+                    ]
+
+          }
+
+        }
+      ]
+    }
+  }
+  response_json = json.dumps(output)
+  return response_json
+  # return flask.jsonify({"results": "output"})
 
 
 if __name__ == "__main__":
